50d2bd17ab81c9f24f56334490b330f838342f25
Timur958-mart <martusenko.timur@mail.ru>
2024-07-04 02:00:11 +0300

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,27 @@
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
+
+
+def string_info(string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+
+
+def is_contains(string, list_to_search):
+    count_calls()
+    string = string.lower()
+    list_to_search = [i.lower() for i in list_to_search]
+
+    if string in list_to_search:
+        return True
+    else:
+        return False
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
+print(calls)
